c11b18433bcdde80a0f91d9733c008178d67e74e
soonbum <ssshhh0402@naver.com>
2019-11-24 23:35:17 +0900

    SB | Movie.Views 시작(index)

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect,get_object_or_404
+from .models import Movie
 # Create your views here.
-def index(request):
-    pass
+def index(request):             # 여기서 갑자기 든 생각인데 User model 한테 watched list  혹은 Movie model한테 watched people list 주면 어떨까 싶습니다.
+    preferences = request.user.preference
+    movie_list = []
+    for idx in preferences:             #유저 preference 중 popularity 높은 애들 추가 다만 여기서 그 뭐냐 본 아이들은 뺀 작업 고려해 봐야 할 듯
+                                        # 만약 넣는다고 하면, objects 받아온거 앞에서부터 하나씩 확인해야할듯
+        movie = Movie.objects.filter(genre = idx).order_by('-popularity')
+        movie_list.append(movie[:10])
+                                            #movie_list에 지금 핫한 탑 10 넣는것도 고려
+    context = {
+        'movie_list' : movie_list
+    }
+    return render(request, 'movies/index', context)
+    
