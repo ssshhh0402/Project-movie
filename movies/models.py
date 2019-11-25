0c52f9f56c41f94951645d6baeda8231bc8bbccc
soonbum <ssshhh0402@naver.com>
2019-11-26 00:01:04 +0900

    SB | Now Movie Edit

--- movies/models.py
+++ movies/models.py
@@ -10,7 +10,8 @@ class Movie(models.Model):
     release_date = models.DateField()
     credit = models.TextField()
     genres = models.TextField()
-    poster_url = models.TextField()
+    poster = models.TextField()
+    videos = models.TextField()
 
 class Genre(models.Model):
     name = models.TextField()
