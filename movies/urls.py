1a3271cd9d85e2aa18322ed36334b50159f32e28
ssshhh0402 <ssshhh0402@naver.com>
2019-11-27 09:27:33 +0900

    SB | LIKES

--- movies/urls.py
+++ movies/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('<int:movie_pk>/', views.detail, name='detail'),
     path('<int:movie_pk>/comment', views.comment_create, name='c_comment'),
+    path('<int:movie_pk>/like', views.like, name='like'),
     
 ]
